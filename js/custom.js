3b48f324076bd1a5443ce573dffda00853b7a7ee
LeeJaMin <sweetmin33@naver.com>
2024-12-29 22:39:25 +0900

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,28 @@
+$(function(){
+   $('.quick-menu ul li').click(function(){
+        $('.quick-menu ul li').removeClass('on');
+        $(this).addClass('on');
+    })
+        
+    $('.tab li a').on('click', function () {
+        // 모든 탭 버튼의 'active' 클래스 제거
+        $('.tab li').removeClass('on');
+        // 클릭한 탭 버튼에 'active' 클래스 추가
+        $(this).addClass('on');
+    
+        // 모든 탭 콘텐츠 숨김
+        $('.tab-panel').removeClass('on');
+        // 클릭한 탭 버튼과 연결된 콘텐츠 표시
+        const target = $(this).data('tab');
+        $('#' + target).addClass('on');
+      });
+  });
 
-
-                    $(function(){
-                       $('.quick-menu ul li').click(function(){
-                            $('.quick-menu ul li').removeClass('on');
-                            $(this).addClass('on');
-                        })
-                            
-                        $('.tabs li a').on('click', function () {
-                            // 모든 탭 버튼의 'active' 클래스 제거
-                            $('.tabs li').removeClass('on');
-                            // 클릭한 탭 버튼에 'active' 클래스 추가
-                            $(this).addClass('on');
-                        
-                            // 모든 탭 콘텐츠 숨김
-                            $('.tab-panel').removeClass('on');
-                            // 클릭한 탭 버튼과 연결된 콘텐츠 표시
-                            const target = $(this).data('tab');
-                            $('#' + target).addClass('on');
-                          });
-                        });
-
-                        $(function(){
-                            $('.review-cont').slideUp();
-                
-                            $('.review').click(function(){
-                
-                                $('.review-cont').stop().slideUp();
-                                $(this).parents().siblings('.review-cont').stop().slideToggle();
-                                $(this).find('i').toggleClass('active');
-                            });
-                        })
+    $(function(){
+        $('.review-cont').slideUp();                
+        $('.review').click(function(){                
+            $('.review-cont').stop().slideUp();
+            $(this).parents().siblings('.review-cont').stop().slideToggle();
+            $(this).find('i').toggleClass('active');
+        });
+    })
